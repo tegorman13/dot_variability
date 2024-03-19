3b858c607ed79568e93229ded4a87e89f69133d2
Thomas <tegorman13@gmail.com>
2024-03-19 18:08:30 -0400

    improve dot sim analysis - restructure site slightly.

--- R/fun_plot.R
+++ R/fun_plot.R
@@ -6,33 +6,99 @@ theme_blank <- theme(
   axis.ticks = element_blank()
 )
 
+
+
+
+
+
 plot_dotsAll <- function(df) {
   plots <- list()
 
   for (i in 1:nrow(df)) {
+    pair_info <- pairCounts |> filter(pair_label==df[i,]$pair_label) |>
+      mutate(across(where(is.numeric), ~round(., 2))) 
+    title= paste0("Mean Rating: ",pair_info$mean_resp, ";   SD: ",pair_info$sd, ";   N Ratings: ",pair_info$n)
+
+
+
+    pat1 <- df[i,] %>%
+          mutate(pattern_1 = purrr::map(pattern_1, jsonlite::fromJSON)) %>%
+          unnest(pattern_1) %>%
+          mutate(y=-y, pat=item_label_1) 
+
+    pat2 <- df[i,] %>%
+          mutate(pattern_2 = purrr::map(pattern_2, jsonlite::fromJSON)) %>%
+          unnest(pattern_2) %>%
+          mutate(y=-y, pat=item_label_2) 
+
+    pat <- rbind(pat1,pat2)
+
+    p1 <- pat |> 
+    ggplot(aes(x = x, y = y)) +
+          geom_point() +
+          coord_cartesian(xlim = c(-25, 25), ylim = c(-25, 25)) +
+          theme_minimal() +
+          facet_wrap(~pat,ncol=2) + theme_blank +
+          labs(title=title) +
+          theme(plot.title = element_text(size = 12,hjust=.5), 
+                strip.text = element_text(size = 7,hjust=.5),
+                panel.spacing.x=unit(-9, "lines"))
+
+  plots <- append(plots, list(p1 ))
+  }
+
+  patchwork::wrap_plots(plots,ncol=1)
+
+}
+
+
+
+plot_dotsAll_old2 <- function(df) {
+  plots <- list()
+
+  for (i in 1:nrow(df)) {
+
+      pair_info <- pairCounts |> filter(pair_label==df[i,]$pair_label)
+      title= paste0("Mean Similarity Rating: ",pair_info$mean_resp, " SD of Similarity Rating: ",pair_info$sd)
+
     p1 <- df[i, ] %>%
       mutate(pattern_1 = purrr::map(pattern_1, jsonlite::fromJSON)) %>%
       unnest(pattern_1) %>%
+       mutate(y=-y) |>
       ggplot(aes(x = x, y = y)) +
       geom_point() +
       coord_cartesian(xlim = c(-25, 25), ylim = c(-25, 25)) +
       theme_minimal() +
-      labs(title = df$item_label_1[i]) + theme_blank
+      labs(title=title, subtitle = df$item_label_1[i]) + theme_blank + 
+      theme(plot.title = element_text(size = 15,hjust=.9,vjust=2))
 
     p2 <- df[i, ] %>%
       mutate(pattern_2 = purrr::map(pattern_2, jsonlite::fromJSON)) %>%
       unnest(pattern_2) %>%
+       mutate(y=-y) |>
       ggplot(aes(x = x, y = y)) +
       geom_point() +
       coord_cartesian(xlim = c(-25, 25), ylim = c(-25, 25)) +
       theme_minimal() +
-      labs(title = df$item_label_2[i]) + theme_blank
+      labs(subtitle = df$item_label_2[i]) + theme_blank + 
+       theme(plot.subtitle = element_text(size = 8))
 
-    plots <- append(plots, list(p1, p2))
+    # design_layout <- "
+    # #3##
+    # 1122
+    # "
+    # p1 + p2 + grid::textGrob(title) + plot_layout(design = design_layout)
+
+    plots <- append(plots, list(p1, p2 ))
   }
-
-  patchwork::wrap_plots(plots,ncol=2)
+  patchwork::wrap_plots(plots,ncol=2) 
 }
+
+
+
+
+
+
 
 
 plot_dotsAll_orig <- function(df) {
@@ -43,6 +109,7 @@ plot_dotsAll_orig <- function(df) {
                  names_to = c(".value", "dot")) %>%
     group_by(id) %>%
     mutate(dot = as.numeric(dot)) %>%
+     mutate(y=-y) |>
     arrange(dot)
   
   plots <- list()
@@ -83,6 +150,7 @@ plot_dots <- function(df) {
   df %>%
     mutate(pattern_1 = purrr::map(pattern_1, jsonlite::fromJSON)) %>%
     unnest(pattern_1) %>%
+     mutate(y=-y) |>
     ggplot(aes(x = x, y = y)) +
     geom_point() +
     coord_cartesian(xlim = c(-25, 25), ylim = c(-25, 25)) +
@@ -93,6 +161,7 @@ plot_dots2 <- function(df) {
   p1 <- df %>%
     mutate(pattern_1 = purrr::map(pattern_1, jsonlite::fromJSON)) %>%
     unnest(pattern_1) %>%
+     mutate(y=-y) |>
     ggplot(aes(x = x, y = y)) +
     geom_point() +
    # coord_fixed(ratio = 1) +
@@ -103,6 +172,7 @@ plot_dots2 <- function(df) {
   p2 <- df %>%
     mutate(pattern_2 = purrr::map(pattern_2, jsonlite::fromJSON)) %>%
     unnest(pattern_2) %>%
+     mutate(y=-y) |>
     ggplot(aes(x = x, y = y)) +
     geom_point() +
     #coord_fixed(ratio = 1) +
@@ -113,3 +183,34 @@ plot_dots2 <- function(df) {
   p1 + p2
 }
 
+
+
+theme_nice <- function() {
+  theme_minimal(base_family = "Manrope") +
+    theme(
+      panel.grid.minor = element_blank(),
+      plot.title = element_text(family = "Manrope Extrabold", face = "plain", size = rel(1.35)),
+      plot.subtitle = element_text(family = "Manrope Medium", face = "plain", size = rel(1.2)),
+      axis.title = element_text(family = "Manrope SemiBold", face = "plain", size = rel(1)),
+      axis.title.x = element_text(hjust = .5),
+      axis.title.y = element_text(hjust = .5),
+      axis.text = element_text(family = "Manrope Light", face = "plain", size = rel(0.8)),
+      strip.text = element_text(
+        family = "Manrope", face = "bold",
+        size = rel(.75), hjust = 0
+      ),
+      strip.background = element_rect(fill = "grey90", color = NA)
+    )
+}
+
+theme_nice_dist <- function() {
+  theme_nice() +
+    theme(
+      panel.grid = element_blank(),
+      panel.spacing.x = unit(10, units = "pt"),
+      axis.ticks.x = element_line(linewidth = 0.25),
+      axis.text.y = element_blank()
+    )
+}
+
+theme_set(theme_nice())
