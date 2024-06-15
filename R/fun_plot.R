8e343aec2b4ad80d58a581c0c23771738747a6ac
Thomas <tegorman13@gmail.com>
2024-06-15 18:24:44 -0400

    update plots

--- R/fun_plot.R
+++ R/fun_plot.R
@@ -187,7 +187,7 @@ plot_dots2 <- function(df) {
 
 
 theme_nice <- function() {
-  theme_minimal(base_family = "Manrope") +
+ # theme_minimal(base_family = "Manrope") +
     theme(
       panel.grid.minor = element_blank(),
       plot.title = element_text(family = "Manrope Extrabold", face = "plain", size = rel(1.35)),
@@ -237,8 +237,29 @@ theme_nice <- function() {
     )
 }
 
+
+theme_nice_pdf <- function() {
+ theme_minimal() +
+    theme(
+      panel.grid.minor = element_blank(),
+      plot.title = element_text( face = "plain", size = rel(1.35)), #family = "Manrope Extrabold",
+      plot.subtitle = element_text(face = "plain", size = rel(1.2)), # family = "Manrope Medium", 
+      axis.title = element_text(face = "plain", size = rel(1)), #family = "Manrope SemiBold", 
+      axis.title.x = element_text(hjust = .5),
+      axis.title.y = element_text(hjust = .5),
+      axis.text = element_text(face = "plain", size = rel(0.8)),
+      strip.text = element_text(
+         face = "bold", #family = "Manrope SemiBold", 
+        size = rel(.95), hjust = 0
+      ),
+      legend.position = "top",
+      legend.text = element_text(face = "plain", size = rel(0.95)), #family = "Manrope Light",
+      strip.background = element_rect(fill = "grey90", color = NA)
+    )
+}
+
 theme_nice_b <- function() {
-  theme_minimal(base_family = "Manrope") +
+  #theme_minimal(base_family = "Manrope") +
     theme(
       panel.grid.minor = element_blank(),
       plot.title = element_text(family = "Manrope Extrabold", face = "plain", size = rel(1.55)),
@@ -246,13 +267,13 @@ theme_nice_b <- function() {
       axis.title = element_text(family = "Manrope SemiBold", face = "plain", size = rel(1)),
       axis.title.x = element_text(hjust = .5),
       axis.title.y = element_text(hjust = .5),
-      axis.text = element_text(family = "Manrope Light", face = "plain", size = rel(0.9)),
+      axis.text = element_text(face = "plain", size = rel(0.9)),
       strip.text = element_text(
         family = "Manrope", face = "bold",
         size = rel(1.2), hjust = 0
       ),
       legend.position = "top",
-      legend.text = element_text(family = "Manrope Light", face = "plain", size = rel(1.3)),
+      legend.text = element_text( face = "plain", size = rel(1.3)), #family = "Manrope Light",
       strip.background = element_rect(fill = "grey90", color = NA)
     )
 }
